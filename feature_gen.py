94f6ba2fb70c6fa77745cd94ada4a239a665eb00
biye2014 <drbiye2014@gmail.com>
2014-09-23 11:50:02 +0200

    kick-off & before lunch

--- feature_gen.py
+++ feature_gen.py
@@ -10,5 +10,25 @@ __author__ = 'jma'
 # generate features from word-bigram and their context
 #
 
-def feature_gen(bigram, left_char_ngram, right_char_ngram):
-    pass
+def feature_gen(previous_word, word, incoming_char):
+    # first 13 features are from word-based CWS, Zhang & Clark (2011)
+    f3 = u'NULL'
+    if len(word) == 1:
+        f3 = u'_l_'.join(word, u'one')
+
+    word_end = word[-1]
+    word_begin = word[0]
+
+    p_word_end = previous_word[-1]
+    p_word_begin = previous_word[0]
+
+
+    #
+    # --> here!
+    #
+    word_len_str = u"".join(str(len(word)))
+    p_word_len_str = u"".join(str(len(previous_word)))
+
+    base_feature_str = [word, u"_".join(previous_word, word), f3, u"_".join(word[0], word_len_str), \
+                        u"".join(word[-1], word_len_str), u"_".join(w - 1[-1])]
+    #unitl f6
